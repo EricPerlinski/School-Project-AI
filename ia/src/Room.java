bbed9068d9c9ab3814dd5ed4442facae27d4371f
Damien <damien.kemberg@telecomnancy.eu>
2016-01-01 16:32:56 +0100

    ajout bases

---
+++ ia/src/Room.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+
+public class Room {
+	
+	private enum Equipment {
+		videoprojecteur,
+		retroprojecteur,
+		salleTP,
+		tableau;
+	}
+	
+	private ArrayList<Availability> roomAvailabilities;
+	private ArrayList<Equipment> roomEquipment;
+	
+	public Room(ArrayList<Availability> roomAvailabilities,
+			ArrayList<Equipment> roomEquipment) {
+		super();
+		this.roomAvailabilities = roomAvailabilities;
+		this.roomEquipment = roomEquipment;
+	}
+
+	public ArrayList<Availability> getRoomAvailabilities() {
+		return roomAvailabilities;
+	}
+
+	public void setRoomAvailabilities(ArrayList<Availability> roomAvailabilities) {
+		this.roomAvailabilities = roomAvailabilities;
+	}
+
+	public ArrayList<Equipment> getRoomEquipment() {
+		return roomEquipment;
+	}
+
+	public void setRoomEquipment(ArrayList<Equipment> roomEquipment) {
+		this.roomEquipment = roomEquipment;
+	}
+	
+	
+	
+	
+
+}
