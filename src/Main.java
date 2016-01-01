3c82e22d3bc524f99319b4e499a39b94deb8d9f7
Eric Perlinski <eric.perlinski@gmail.com>
2016-01-01 20:43:22 +0100

    Refactoring, added scenario with basicScenario method

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Teacher t1 = new Teacher();
-		Teacher t2 = new Teacher();
-		Teacher t3 = new Teacher();
-		basicScenario(t1,t2,t3);
+		Scenario S1 = new Scenario();
+		S1.basicScenario();
 	
 	}
 	
-	public static void basicScenario(Teacher t1, Teacher t2, Teacher t3){
-		// Creating availabilities
-		ArrayList<Availability> u1 = new ArrayList<Availability>();
-		u1.add(new Availability(Availability.weekDays.j1, Availability.beginHour.SIXTEEN));
-		u1.add(new Availability(Availability.weekDays.j2, Availability.beginHour.FOURTEEN));
-		
-		ArrayList<Availability> u2 = new ArrayList<Availability>();
-		u2.add(new Availability(Availability.weekDays.j1, Availability.beginHour.SIXTEEN));
-		u2.add(new Availability(Availability.weekDays.j2, Availability.beginHour.TEN));
-		
-		ArrayList<Availability> u3 = new ArrayList<Availability>();
-		u3.add(new Availability(Availability.weekDays.j1, Availability.beginHour.FOURTEEN));
-		u3.add(new Availability(Availability.weekDays.j2, Availability.beginHour.EIGHT));
-		
-		
-		//Creating new teachers
-		t1 = new Teacher (u1);
-		t2 = new Teacher (u2);
-		t3 = new Teacher (u3);
-		
-		//Adding the skills 
-		t1.setSkills(new ArrayList<Teaching>(Arrays.asList(new Teaching("English", 12))));
-		t2.setSkills(new ArrayList<Teaching>(Arrays.asList(new Teaching("ComputerScience", 12))));
-		t3.setSkills(new ArrayList<Teaching>(Arrays.asList(new Teaching("Mathematics", 12))));
-		
-		//Adding needed material
-		t1.setMaterial(new ArrayList<Equipment>(Arrays.asList(Equipment.tableau,Equipment.retroprojecteur)));
-		t2.setMaterial(new ArrayList<Equipment>(Arrays.asList(Equipment.salleTP,Equipment.tableau,Equipment.videoprojecteur)));
-		t3.setMaterial(new ArrayList<Equipment>(Arrays.asList(Equipment.retroprojecteur,Equipment.tableau,Equipment.videoprojecteur)));
-		
-		//Creating Rooms
-		//TODO
-	
-	}
 }
---
+++ src/Scenario.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Scenario {
+	
+	private ArrayList<Teacher> AT;
+	private ArrayList<Room> AR;
+	
+	
+	/* Constructors */ 
+	public Scenario(){
+		super();
+		this.setAT(new ArrayList<Teacher>());
+		this.setAR(new ArrayList<Room>());
+	}
+
+	public void basicScenario(){
+		
+		// Creating availabilities
+		ArrayList<Availability> u1 = new ArrayList<Availability>();
+		u1.add(new Availability(Availability.weekDays.j1, Availability.beginHour.SIXTEEN));
+		u1.add(new Availability(Availability.weekDays.j2, Availability.beginHour.FOURTEEN));
+		
+		ArrayList<Availability> u2 = new ArrayList<Availability>();
+		u2.add(new Availability(Availability.weekDays.j1, Availability.beginHour.SIXTEEN));
+		u2.add(new Availability(Availability.weekDays.j2, Availability.beginHour.TEN));
+		
+		ArrayList<Availability> u3 = new ArrayList<Availability>();
+		u3.add(new Availability(Availability.weekDays.j1, Availability.beginHour.FOURTEEN));
+		u3.add(new Availability(Availability.weekDays.j2, Availability.beginHour.EIGHT));
+		
+		
+		//Creating new teachers
+		Teacher t1 = new Teacher (u1);
+		Teacher t2 = new Teacher (u2);
+		Teacher t3 = new Teacher (u3);
+		
+		//Adding the skills 
+		t1.setSkills(new ArrayList<Teaching>(Arrays.asList(new Teaching("English", 12))));
+		t2.setSkills(new ArrayList<Teaching>(Arrays.asList(new Teaching("ComputerScience", 12))));
+		t3.setSkills(new ArrayList<Teaching>(Arrays.asList(new Teaching("Mathematics", 12))));
+		
+		//Adding needed material
+		t1.setMaterial(new ArrayList<Equipment>(Arrays.asList(Equipment.tableau,Equipment.retroprojecteur)));
+		t2.setMaterial(new ArrayList<Equipment>(Arrays.asList(Equipment.salleTP,Equipment.tableau,Equipment.videoprojecteur)));
+		t3.setMaterial(new ArrayList<Equipment>(Arrays.asList(Equipment.retroprojecteur,Equipment.tableau,Equipment.videoprojecteur)));
+		
+		AT.add(t1);
+		AT.add(t2);
+		AT.add(t3);
+		
+		//Creating Rooms
+		//TODO
+	
+	}
+	
+	
+	/* Setters & Getters */
+	public ArrayList<Room> getAR() {
+		return AR;
+	}
+
+	public void setAR(ArrayList<Room> aR) {
+		AR = aR;
+	}
+
+	public ArrayList<Teacher> getAT() {
+		return AT;
+	}
+
+	public void setAT(ArrayList<Teacher> aT) {
+		AT = aT;
+	}
+	
+	
+}
